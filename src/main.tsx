c644754bb4986f1b5967e7d296000ef4ae26980e
seongjaehong <sjhong310@gmail.com>
2025-11-27 21:01:55 +0900

    chore: export router

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const router = createRouter({ routeTree });
+export const router = createRouter({ routeTree });
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
