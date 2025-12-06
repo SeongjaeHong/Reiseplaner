d3710285918903dca3d9a20e702e724218551643
seongjaehong <sjhong310@gmail.com>
2025-12-06 17:37:50 +0900

    chore: remove fetchkey based cache data modification

--- src/components/plan/ChangePlanNamePopupBox.tsx
+++ src/components/plan/ChangePlanNamePopupBox.tsx
@@ -1,37 +1,22 @@
 import InputPopupBox from '@/components/popupBoxes/InputPopupBox';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { renamePlanByPlanId, type TypePlans } from '@/apis/supabase/plans';
+import { useMutation } from '@tanstack/react-query';
+import { renamePlanByPlanId } from '@/apis/supabase/plans';
 
 type ChangePlanNamePopupBoxParams = {
   planId: number;
-  fetchKey: string[];
   onClose: () => void;
-  refetch?: () => Promise<unknown>;
+  refetch: () => Promise<unknown>;
 };
 
 export default function ChangePlanNamePopupBox({
   planId,
-  fetchKey,
   onClose,
   refetch,
 }: ChangePlanNamePopupBoxParams) {
-  const queryClient = useQueryClient();
-
-  const updateTitle = (title: string) => {
-    queryClient.setQueryData(fetchKey, (plans: TypePlans) =>
-      plans.map((plan) =>
-        plan.id === planId ? { ...plan, title: title } : plan
-      )
-    );
-  };
-
   const { mutate: changePlanName } = useMutation({
     mutationFn: (title: string) => renamePlanByPlanId(planId, title),
-    onSuccess: async (_, title) => {
-      updateTitle(title);
-      if (refetch) {
-        await refetch();
-      }
+    onSuccess: async () => {
+      await refetch();
     },
   });
 
