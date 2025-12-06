afa0a96d58ec45b5f003f474ad482efb789b6b25
seongjaehong <sjhong310@gmail.com>
2025-12-06 17:36:03 +0900

    chore: add type assertion

--- src/routes/-constant.ts
+++ src/routes/-constant.ts
@@ -1,4 +1,4 @@
 export const INDEX = '/' as const;
 export const PLAN_GROUP = '/plangroup' as const;
-export const PLAN = PLAN_GROUP + '/plan';
+export const PLAN = `${PLAN_GROUP}/plan` as const;
 export const NEW_PLAN = '/newplan' as const;
