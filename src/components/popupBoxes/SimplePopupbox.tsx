02c6b195565cc7b0ea58b6d098c5a2673430b49e
seongjaehong <sjhong310@gmail.com>
2025-12-06 17:02:00 +0900

    feat: add simple message popup box

---
+++ src/components/popupBoxes/SimplePopupbox.tsx
@@ -0,0 +1,22 @@
+type SimplePopupbox = {
+  text: string;
+  onAccept: () => void;
+};
+
+export default function SimplePopupbox({ text, onAccept }: SimplePopupbox) {
+  return (
+    <div className='fixed z-1 top-50 left-1/2 -translate-x-1/2 py-2 px-3 w-70 bg-zinc-100 border-2 border-reiseorange rounded-md'>
+      <div>
+        <span className='text-reiseorange text-sm font-bold'>{text}</span>
+      </div>
+      <div className='flex justify-center pt-3 w-full'>
+        <button
+          className='py-1 px-5 mx-3 rounded-lg bg-reisered'
+          onClick={() => void onAccept()}
+        >
+          OK
+        </button>
+      </div>
+    </div>
+  );
+}
