65316a3dac388a333c64d55d1c49b68f9e14c7e9
seongjaehong <sjhong310@gmail.com>
2025-12-10 03:05:00 +0900

    feat: add a delete button

--- src/components/planContents/ImageBox.tsx
+++ src/components/planContents/ImageBox.tsx
@@ -2,16 +2,37 @@ import type { Content, ImageContent } from '@/apis/supabase/planContents';
 import { useRef, useState } from 'react';
 import { ResizableBox, type ResizeCallbackData } from 'react-resizable';
 import 'react-resizable/css/styles.css';
+import { FaRegTrashCan } from 'react-icons/fa6';
+import { deletePlanGroupThumbnail } from '@/apis/supabase/buckets';
 
 type ImageBox = {
   content: ImageContent;
   updateContents: (content: Content) => Promise<void>;
 };
 const PROJECT_ID = import.meta.env.VITE_PROJECT_ID as string;
-
+const defaultMaxLength = 400;
 export default function ImageBox({ content, updateContents }: ImageBox) {
-  const [width, setWidth] = useState(content.width ?? 200);
-  const [height, setHeight] = useState(content.height ?? 200);
+  let contentWidth: number;
+  let contentHeight: number;
+
+  if (content.width > content.height && content.width > defaultMaxLength) {
+    const ratio = content.height / content.width;
+    contentWidth = defaultMaxLength;
+    contentHeight = ratio * contentWidth;
+  } else if (
+    content.height > content.width &&
+    content.height > defaultMaxLength
+  ) {
+    const ratio = content.width / content.height;
+    contentHeight = defaultMaxLength;
+    contentWidth = ratio * contentHeight;
+  } else {
+    contentWidth = content.width;
+    contentHeight = content.height;
+  }
+
+  const [width, setWidth] = useState(contentWidth);
+  const [height, setHeight] = useState(contentHeight);
   const refUpdateTimer = useRef<number | null>(null);
 
   const handleResize = (
@@ -29,19 +50,32 @@ export default function ImageBox({ content, updateContents }: ImageBox) {
     }, 1000);
   };
 
+  const handleDeleteContent = async () => {
+    await updateContents({ ...content, data: '' });
+    await deletePlanGroupThumbnail(content.data);
+  };
+
   return (
     <ResizableBox
       width={width}
       height={height}
       onResize={handleResize}
       axis='both'
-      className='w-full'
+      className='group relative w-full'
     >
       <img
         src={`https://${PROJECT_ID}.supabase.co/storage/v1/object/public/${content.data}`}
         alt='Image'
         className='w-full h-full object-contain rounded-lg'
       />
+      <div className='absolute top-0 right-0 bg-reiseorange rounded-full w-6 h-6 text-center invisible group-hover:visible'>
+        <button
+          onClick={() => void handleDeleteContent()}
+          className='text-white text-lg'
+        >
+          <FaRegTrashCan />
+        </button>
+      </div>
     </ResizableBox>
   );
 }
