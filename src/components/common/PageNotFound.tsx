c59ad190adc234606a41c6b5785b886d36ca042f
seongjaehong <sjhong310@gmail.com>
2025-12-20 19:18:30 +0900

    feat: add a not found page

---
+++ src/components/common/PageNotFound.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from '@tanstack/react-router';
+
+export default function PageNotFound() {
+  const navigate = useNavigate();
+  const handleNavigateHome = () => void navigate({ to: '/' });
+
+  return (
+    <div className='absolute top-1/5 left-1/2 -translate-x-1/2 p-5 text-reisered font-bold'>
+      <h1 className='text-6xl mb-5'>Reiseplaner</h1>
+      <p className='text-xl mb-5'>The requested URL was not found.</p>
+      <div className='text-center'>
+        <button
+          onClick={handleNavigateHome}
+          className='bg-reisered hover:bg-red-500 text-white rounded-md py-1 px-2'
+        >
+          Go back home
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,12 @@ import './index.css';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import PageNotFound from './components/common/PageNotFound';
 
-export const router = createRouter({ routeTree });
+export const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: () => <PageNotFound />,
+});
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
