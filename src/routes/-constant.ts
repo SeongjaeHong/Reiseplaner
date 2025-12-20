8a4eb50a035592346f0070cd62cc0e6fc69c3d1e
seongjaehong <sjhong310@gmail.com>
2025-12-20 19:39:58 +0900

    chore: remove an unused path and add slash at the plan group index route

--- src/routes/-constant.ts
+++ src/routes/-constant.ts
@@ -1,4 +1,3 @@
 export const INDEX = '/' as const;
-export const PLAN_GROUP = '/plangroup' as const;
-export const PLAN = `${PLAN_GROUP}/plan` as const;
-export const NEW_PLAN = '/newplan' as const;
+export const PLAN_GROUP = '/plangroup/' as const;
+export const PLAN = `${PLAN_GROUP}plan` as const;
