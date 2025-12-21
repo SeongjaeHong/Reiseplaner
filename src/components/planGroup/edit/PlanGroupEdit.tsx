edffd4810028a3ea52d7d493a11a20f6df6d1f6d
SeongjaeHong <sjhong310@gmail.com>
2025-12-22 01:36:36 +0900

    refactor: implement Zod schema validation with real-time feedback in PlanGroupEdit

--- src/components/planGroup/edit/PlanGroupEdit.tsx
+++ src/components/planGroup/edit/PlanGroupEdit.tsx
@@ -1,149 +1,157 @@
 import { Controller, useForm, useWatch } from 'react-hook-form';
-import { useMutation } from '@tanstack/react-query';
-import { deleteImage, uploadImage } from '@/apis/supabase/buckets';
-import { useReducer, useState } from 'react';
-import { updatePlanGroupByGroupId } from '@/apis/supabase/planGroups';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMemo, useReducer } from 'react';
 import type { Database } from '@/database.types';
 import ThumbnailEdit from './ThumbnailEdit';
 import Calendar from './Calendar';
-import type { DateRange } from 'react-day-picker';
 import { getSchedule } from '../utils/time';
+import z from 'zod';
+import type { DateRange } from 'react-day-picker';
+import { usePlanGroupUpdate } from './utils/usePlanGroupUpdate';
+import { isDefaultImage } from '@/apis/supabase/buckets';
 
-type PlanGroupForm = {
-  title: string;
-  thumbnail: File | null;
-  schedule: DateRange | undefined;
-};
+const planGroupEditFormSchema = z.object({
+  title: z.string().trim().min(1, 'Enter a title.'),
+  thumbnail: z.instanceof(File).nullable(),
+  schedule: z
+    .object({
+      from: z.union([z.date(), z.undefined()]), // key 'from' must exist but its value can be undefined
+      to: z.date().optional(),
+    })
+    .optional(),
+});
+export type PlanGroupForm = z.infer<typeof planGroupEditFormSchema>;
+
 type PlanGroupEdit = {
   planGroup: Database['public']['Tables']['plangroups']['Row'];
   thumbnail: File | null;
   onClose: () => void;
   refetch: () => Promise<unknown>;
 };
-export default function PlanGroupEdit({
-  planGroup,
-  thumbnail,
-  onClose,
-  refetch,
-}: PlanGroupEdit) {
+export default function PlanGroupEdit({ planGroup, thumbnail, onClose, refetch }: PlanGroupEdit) {
   const {
     register,
     handleSubmit,
     control,
-    formState: { dirtyFields },
+    trigger,
+    formState: { dirtyFields, errors, isValid },
   } = useForm<PlanGroupForm>({
+    resolver: zodResolver(planGroupEditFormSchema),
+    mode: 'onChange',
     defaultValues: {
       title: planGroup.title,
       thumbnail: thumbnail,
       schedule: {
         from: planGroup.start_time ? new Date(planGroup.start_time) : undefined,
         to: planGroup.end_time ? new Date(planGroup.end_time) : undefined,
-      },
+      } as DateRange,
     },
   });
-  const currentThumbnail = useWatch({ control, name: 'thumbnail' });
-  const currentSchedule = useWatch({ control, name: 'schedule' });
 
-  const isThumbnailDirty = !(currentThumbnail === thumbnail);
-  const isStartTimeDirty =
-    currentSchedule?.from?.toISOString() !==
-    (planGroup.start_time
-      ? new Date(planGroup.start_time).toISOString()
-      : undefined);
-  const isEndTimeDirty =
-    currentSchedule?.to?.toISOString() !==
-    (planGroup.end_time
-      ? new Date(planGroup.end_time).toISOString()
-      : undefined);
-  const isScheduleDirty = isStartTimeDirty || isEndTimeDirty;
+  const watchedThumbnail = useWatch({ control, name: 'thumbnail' });
+  const watchedSchedule = useWatch({ control, name: 'schedule' });
 
-  const isFormDirty =
-    !!dirtyFields.title || isScheduleDirty || isThumbnailDirty;
+  const isThumbnailChanged = useMemo(() => {
+    if (!watchedThumbnail) {
+      if (!thumbnail) {
+        return false;
+      }
+      return !isDefaultImage(thumbnail.name);
+    }
 
-  const { onChange: registerTitleOnChange, ...registerTitleRest } =
-    register('title');
-  const [titleEmpty, setTitleEmpty] = useState(false);
+    if (!thumbnail) {
+      return true;
+    }
 
-  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTitleEmpty(e.currentTarget.value.trim() === '');
-  };
+    return (
+      watchedThumbnail.name !== thumbnail.name ||
+      watchedThumbnail.size !== thumbnail.size ||
+      watchedThumbnail.lastModified !== thumbnail.lastModified
+    );
+  }, [watchedThumbnail, thumbnail]);
 
   const [showCalendar, toggleShowCalendar] = useReducer((prev) => !prev, false);
-  const scheduleText = getSchedule(currentSchedule);
+  const scheduleText = getSchedule(watchedSchedule);
 
-  const { mutate: submit, isPending } = useMutatePlanGroup({
-    planGroup,
-    isThumbnailDirty,
+  const { mutate: submit, isPending } = usePlanGroupUpdate({
+    planGroupId: planGroup.id,
     prevThumbnail: thumbnail,
-    curThumbnail: currentThumbnail,
     onClose,
     refetch,
   });
 
-  const isSubmitDisabled = !isFormDirty || isPending || titleEmpty;
-
   const onFormSubmit = (e: React.FormEvent) => {
-    void handleSubmit((value) => submit(value))(e);
+    void handleSubmit(
+      (value) => submit(value),
+      (error) => {
+        console.log('Validity check error:', error);
+      }
+    )(e);
   };
 
+  const isDirty = isThumbnailChanged || !!dirtyFields.schedule || !!dirtyFields.title;
+  const isSubmitDisabled = !isDirty || !isValid || isPending;
+
   return (
-    <div className='fixed z-1 w-screen h-screen'>
+    <div className="fixed z-1 w-screen h-screen">
       <form
         onSubmit={onFormSubmit}
-        className='fixed flex top-50 left-1/2 -translate-x-1/2 w-3/5 h-70 rounded-sm p-2 bg-reiseorange xl:w-1/3'
+        className="fixed flex top-50 left-1/2 -translate-x-1/2 w-3/5 h-70 rounded-sm p-2 bg-reiseorange xl:w-1/3"
       >
+        {/* Thumbnail area */}
         <Controller
-          name='thumbnail'
+          name="thumbnail"
           control={control}
-          render={({ field: { onChange } }) => (
+          render={({ field: { value, onChange } }) => (
             <ThumbnailEdit
-              image={currentThumbnail}
-              onChange={(file: File | null) => onChange(file)}
+              image={value ?? null}
+              onChange={(file: File | null) => {
+                onChange(file);
+                void trigger('thumbnail');
+              }}
             />
           )}
         />
-        <div className='flex flex-col justify-between w-1/2'>
+        <div className="flex flex-col justify-between w-1/2">
           <div>
-            <div className='relative mb-10'>
-              <div className='rounded-sm border-1'>
+            <div className="relative mb-10">
+              <div className="rounded-sm border-1">
+                {/* Title input area */}
                 <input
-                  type='text'
+                  type="text"
                   defaultValue={planGroup.title}
-                  {...registerTitleRest}
-                  onChange={(e) => {
-                    void registerTitleOnChange(e);
-                    handleTitleChange(e);
-                  }}
-                  className='w-full px-1 text-lg font-bold'
+                  {...register('title')}
+                  className="w-full px-1 text-lg font-bold"
                 />
               </div>
-              {titleEmpty && (
-                <div className='absolute'>
-                  <span className='text-rose-500'>Enter a title</span>
+              {errors.title && (
+                <div className="absolute">
+                  <span className="text-rose-500">{errors.title.message}</span>
                 </div>
               )}
             </div>
 
-            <div className='inline rounded-lg border-1 hover:bg-zinc-300'>
+            {/* Plan schedule area */}
+            <div className="inline rounded-lg border-1 hover:bg-zinc-300">
               <button
                 onClick={(e) => {
                   e.preventDefault();
                   toggleShowCalendar();
                 }}
                 disabled={showCalendar}
-                className='inline-flex gap-1 py-1 px-3 text-xs'
+                className="inline-flex gap-1 py-1 px-3 text-xs"
               >
                 <span>{scheduleText}</span>
               </button>
             </div>
             {showCalendar && (
-              <div className='absolute top-27 left-1/2 -translate-x-1/2 z-1'>
+              <div className="absolute top-27 left-1/2 -translate-x-1/2 z-1">
                 <Controller
-                  name='schedule'
+                  name="schedule"
                   control={control}
                   render={({ field: { onChange } }) => (
                     <Calendar
-                      range={currentSchedule}
+                      range={watchedSchedule}
                       setRange={onChange}
                       onClose={toggleShowCalendar}
                     />
@@ -153,16 +161,13 @@ export default function PlanGroupEdit({
             )}
           </div>
 
-          <div className='flex justify-center gap-2'>
-            <button
-              type='button'
-              onClick={onClose}
-              className='py-1 px-2 rounded-lg bg-red-400'
-            >
+          {/* Botton button area */}
+          <div className="flex justify-center gap-2">
+            <button type="button" onClick={onClose} className="py-1 px-2 rounded-lg bg-red-400">
               Zurück
             </button>
             <button
-              type='submit'
+              type="submit"
               disabled={isSubmitDisabled}
               className={`py-1 px-2 rounded-lg bg-green-300 ${
                 isSubmitDisabled ? 'opacity-50 cursor-not-allowed' : ''
@@ -175,62 +180,4 @@ export default function PlanGroupEdit({
       </form>
     </div>
   );
-}
-
-type UseMutatePlanGroup = {
-  planGroup: Database['public']['Tables']['plangroups']['Row'];
-  isThumbnailDirty: boolean;
-  prevThumbnail: File | null;
-  curThumbnail: File | null;
-  onClose: () => void;
-  refetch: () => Promise<unknown>;
-};
-// Update information of a plan group
-function useMutatePlanGroup({
-  planGroup,
-  isThumbnailDirty,
-  prevThumbnail,
-  curThumbnail,
-  onClose,
-  refetch,
-}: UseMutatePlanGroup) {
-  return useMutation({
-    mutationFn: async (data: PlanGroupForm) => {
-      let thumbnailPath: string | null = null;
-      if (isThumbnailDirty) {
-        // Save a new thumbnail image in DB.
-        if (curThumbnail) {
-          const res = await uploadImage(curThumbnail).catch((e) => {
-            console.error('Fail to upload a new image in db.');
-            console.error(e);
-            throw e;
-          });
-          thumbnailPath = res.fullPath;
-        } else {
-          thumbnailPath = null;
-        }
-
-        // Remove a previously saved thumbnail image from DB.
-        if (prevThumbnail) {
-          await deleteImage(prevThumbnail.name).catch((e) => {
-            console.error('Fail to remove an image from db.');
-            console.error(e);
-          });
-        }
-      }
-
-      // Update a title and a thumbnail path of a plan group in DB
-      await updatePlanGroupByGroupId(
-        planGroup.id,
-        data.title,
-        thumbnailPath,
-        data.schedule?.from?.toISOString() ?? null,
-        data.schedule?.to?.toISOString() ?? null
-      );
-    },
-    onSuccess: async () => {
-      await refetch();
-      onClose();
-    },
-  });
 }
