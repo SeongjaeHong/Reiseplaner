5bd23e80d31fc8fc97971441d63b70c945763e1a
SeongjaeHong <sjhong310@gmail.com>
2025-12-22 21:20:13 +0900

    feat: add an error page

---
+++ src/errors/UnhandledError.tsx
@@ -0,0 +1,22 @@
+import { INDEX } from '@/routes/-constant';
+import { useNavigate } from '@tanstack/react-router';
+
+export default function UnhandledError() {
+  const navigate = useNavigate();
+  const handleNavigateHome = () => void navigate({ to: INDEX });
+
+  return (
+    <div className='text-reisered absolute top-1/5 left-1/2 -translate-x-1/2 p-5 font-bold'>
+      <h1 className='mb-5 text-6xl max-sm:text-4xl'>Reiseplaner</h1>
+      <p className='mb-5 text-xl max-sm:text-lg'>Something went wrong.</p>
+      <div className='text-center'>
+        <button
+          onClick={handleNavigateHome}
+          className='bg-reisered rounded-md px-2 py-1 text-white hover:bg-red-500'
+        >
+          Go back home
+        </button>
+      </div>
+    </div>
+  );
+}
