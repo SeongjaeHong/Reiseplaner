b0aaba6c01f0c77e7f0709f18e58d5619ab87750
SeongjaeHong <sjhong310@gmail.com>
2025-12-22 20:50:23 +0900

    feat: return null if fails to fetch an image

--- src/components/planGroup/PlanGroup.tsx
+++ src/components/planGroup/PlanGroup.tsx
@@ -7,6 +7,7 @@ import type { Database } from '@/database.types';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { downloadImage } from '@/apis/supabase/buckets';
 import { getSchedule } from './utils/time';
+import { toast } from '../common/Toast/toast';
 
 type typePlanGroup = {
   to: string;
@@ -51,36 +52,38 @@ export default function PlanGroup({ to, planGroup, refetch }: typePlanGroup) {
 
   return (
     <>
-      <Link
-        to={to}
-        search={{ group_id: planGroup.id, group_title: planGroup.title }}
-      >
-        <div className='group relative flex h-60 bg-reisered truncate'>
+      <Link to={to} search={{ group_id: planGroup.id, group_title: planGroup.title }}>
+        <div className='group bg-reisered relative flex h-60 truncate'>
           <div className='w-1/3 flex-shrink-0'>
-            <img
-              src={URL.createObjectURL(thumbnail)}
-              alt='A thumbnail of a plan group'
-              className='h-full w-full object-fill'
-            />
+            {thumbnail && (
+              <img
+                src={URL.createObjectURL(thumbnail)}
+                alt='A thumbnail of a plan group'
+                className='h-full w-full object-fill'
+              />
+            )}
+            {!thumbnail && (
+              <div className='h-full w-full bg-zinc-500 object-fill'>
+                <p className='text-white'>Failed to load an image.</p>
+              </div>
+            )}
           </div>
-          <div className='flex-1 font-bold mx-2'>
+          <div className='mx-2 flex-1 font-bold'>
             <div className='py-2'>
               <h1
                 title={planGroup.title}
-                className='text-2xl max-md:text-xl line-clamp-2 whitespace-normal break-all'
+                className='line-clamp-2 text-2xl break-all whitespace-normal max-md:text-xl'
               >
                 {planGroup.title}
               </h1>
             </div>
             <div className='inline-block rounded-lg border-1 px-2'>
-              <span className='text-sm max-md:text-xs whitespace-normal'>
-                {schedule}
-              </span>
+              <span className='text-sm whitespace-normal max-md:text-xs'>{schedule}</span>
             </div>
           </div>
-          <div className='absolute right-1 invisible group-hover:visible overflow-visible'>
+          <div className='invisible absolute right-1 overflow-visible group-hover:visible'>
             <button
-              className='hover:bg-green-300 rounded-full p-2'
+              className='rounded-full p-2 hover:bg-green-300'
               onMouseEnter={handleMenuMouseEnter}
               onMouseLeave={handleMenuMouseLeave}
               onClick={(e) => e.preventDefault()}
@@ -119,9 +122,7 @@ export default function PlanGroup({ to, planGroup, refetch }: typePlanGroup) {
   );
 }
 
-const StyleMenuGroup = (
-  showMenu: boolean
-) => `absolute invisible left-[-90px] top-[-30px]
+const StyleMenuGroup = (showMenu: boolean) => `absolute invisible left-[-90px] top-[-30px]
       duration-200 ease-out opacity-0 bg-zinc-500/0
       text-center
       divide-y divide-zinc-600
@@ -166,7 +167,14 @@ type UseFetchImage = {
 function useFetchImage({ imageURL }: UseFetchImage) {
   const { data } = useSuspenseQuery({
     queryKey: [imageURL],
-    queryFn: () => downloadImage(imageURL),
+    queryFn: () => {
+      try {
+        return downloadImage(imageURL);
+      } catch {
+        toast.error('Failed to download an image from the server.');
+        return null;
+      }
+    },
     staleTime: Infinity,
   });
 
