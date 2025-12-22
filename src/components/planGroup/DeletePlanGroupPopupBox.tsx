ce961dfa73ebed7e937172f6414b9aaa8a132afc
SeongjaeHong <sjhong310@gmail.com>
2025-12-22 20:51:11 +0900

    feat: ignore image in case of null

--- src/components/planGroup/DeletePlanGroupPopupBox.tsx
+++ src/components/planGroup/DeletePlanGroupPopupBox.tsx
@@ -2,10 +2,12 @@ import { useMutation } from '@tanstack/react-query';
 import { deletePlanGroups } from '@/apis/supabase/planGroups';
 import Popupbox from '@/components/common/popupBoxes/Popupbox';
 import { deleteImage } from '@/apis/supabase/buckets';
+import { toast } from '../common/Toast/toast';
+import { ApiError } from '@/errors/ApiError';
 
 type DeletePlanGroupPopupBoxParam = {
   planGroupId: number;
-  thumbnail: File;
+  thumbnail: File | null;
   onClose: () => void;
   refetch: () => Promise<unknown>;
 };
@@ -18,31 +20,33 @@ export default function DeletePlanGroupPopupBox({
 }: DeletePlanGroupPopupBoxParam) {
   const { mutate } = useMutation({
     mutationFn: async () => {
-      await deleteImage(thumbnail.name);
-      return await deletePlanGroups(planGroupId);
-    },
-    onSuccess: async (res) => {
-      {
-        if (res) {
-          await refetch();
-        } else {
-          console.error('Fail to delete the plan group: ', planGroupId);
+      if (thumbnail) {
+        try {
+          await deleteImage(thumbnail.name);
+        } catch {
+          console.error('Failed to delete an image from the server.');
         }
-        onClose();
+      }
+      const success = await deletePlanGroups(planGroupId);
+      if (!success) {
+        throw new ApiError('DATABASE', {
+          message: `Failed to delete the plan group: ${planGroupId}`,
+        });
       }
     },
-    throwOnError: true,
+    onSuccess: async () => {
+      await refetch();
+      onClose();
+    },
+    onError: (error) => {
+      toast.error(error.message);
+      throw error;
+    },
   });
 
   const handleClickCancel = () => {
     onClose();
   };
 
-  return (
-    <Popupbox
-      text='Remove this plan.'
-      onAccept={mutate}
-      onCancel={handleClickCancel}
-    />
-  );
+  return <Popupbox text='Remove this plan.' onAccept={mutate} onCancel={handleClickCancel} />;
 }
