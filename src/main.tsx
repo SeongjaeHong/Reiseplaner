da94e5b1429b3c5e433926b95e79a65bee0d98c6
SeongjaeHong <sjhong310@gmail.com>
2025-12-22 21:20:57 +0900

    feat: add error boundary at the root

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,37 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import PageNotFound from './components/common/PageNotFound';
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import PageNotFound from './errors/PageNotFound';
 import { ToastContainer } from './components/common/Toast/ToastContainer';
+import { ErrorBoundary } from 'react-error-boundary';
+import UnhandledError from './errors/UnhandledError';
+import { logError } from './errors/log';
 
 export const router = createRouter({
   routeTree,
   defaultNotFoundComponent: () => <PageNotFound />,
 });
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onError: (error) => {
+      console.error('Mutation Error:', error);
+    },
+  }),
+  queryCache: new QueryCache({
+    onError: (error) => {
+      console.error('Query Error:', error);
+    },
+  }),
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-    <ToastContainer />
+    <ErrorBoundary fallback={<UnhandledError />} onError={logError}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+      <ToastContainer />
+    </ErrorBoundary>
   </StrictMode>
 );
