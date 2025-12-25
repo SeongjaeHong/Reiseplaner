67e3f5ed112b9eb2e53f041dc659bd41021d8034
SeongjaeHong <sjhong310@gmail.com>
2025-12-25 22:02:50 +0900

    chore: change textcontent to content

--- src/components/planContents/utils/text.ts
+++ src/components/planContents/utils/text.ts
@@ -1,9 +1,8 @@
 import { v4 as uuid } from 'uuid';
-import type { LocalContent } from './contents';
-import type { TextContent } from '@/apis/supabase/planContents.types';
+import type { Content } from '@/apis/supabase/planContents.types';
 
 type UseAddText = {
-  updateLocalContents: (content: LocalContent) => void;
+  updateLocalContents: (content: Content) => void;
   setEditingId: React.Dispatch<React.SetStateAction<string | null>>;
 };
 export function useAddText({ updateLocalContents, setEditingId }: UseAddText) {
@@ -13,7 +12,7 @@ export function useAddText({ updateLocalContents, setEditingId }: UseAddText) {
       end: { hour: null, minute: null },
     };
 
-    const newContent: TextContent = {
+    const newContent: Content = {
       id: uuid(),
       type: 'text',
       title: '',
