1904ba379b525fda1340dfd75f9fb66522629ec6
SeongjaeHong <sjhong310@gmail.com>
2025-12-28 00:22:22 +0900

    feat: add auth functions

---
+++ src/apis/supabase/auth.ts
@@ -0,0 +1,58 @@
+import supabase from '@/supabaseClient';
+import type { Provider } from '@supabase/supabase-js';
+
+export const signUp = async (email: string, password: string) => {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+};
+
+export const signIn = async (email: string, password: string) => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+};
+
+export const signInWithOAuth = async (provider: Provider = 'google') => {
+  await supabase.auth.signInWithOAuth({
+    provider: provider,
+    options: {
+      redirectTo: 'http://localhost:5173/',
+    },
+  });
+};
+
+export const signOut = async () => {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const getUser = async () => {
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return user;
+};
