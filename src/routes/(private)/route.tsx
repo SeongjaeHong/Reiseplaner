228b44902c328ce2a9400530c54edc22530ec817
SeongjaeHong <sjhong310@gmail.com>
2025-12-28 20:54:15 +0900

    chore: change title and name style

--- src/routes/(private)/route.tsx
+++ src/routes/(private)/route.tsx
@@ -16,30 +16,34 @@ function RouteComponent() {
     <>
       <div className='bg-reiseblue flex h-20 items-center justify-between px-5'>
         <Link to={'/'}>
-          <h1 className='text-5xl font-bold max-[430px]:text-4xl'>Reiseplaner</h1>
+          <h1 className='text-5xl font-bold max-[540px]:text-4xl'>Reiseplaner</h1>
         </Link>
-        <div className='flex items-center gap-2'>
-          <p className='font-bold text-orange-500'>{name}</p>
+        <div className='flex w-50 items-center justify-end gap-2 max-[540px]:text-sm'>
+          <p className='max truncate font-bold text-orange-500 max-[470px]:w-30 max-[420px]:hidden'>
+            {name}
+          </p>
           {user ? (
             <div className='relative'>
               <button onClick={toggleShowBox} className='bg-reiseorange rounded-full p-2 text-lg'>
                 <FaUser />
               </button>
-              <div className='absolute -left-52 w-60 rounded-md bg-zinc-600'>
-                <ul>
-                  <li className='truncate border-b-1 border-zinc-400 px-2 py-1'>{name}</li>
-                  <li className='flex items-center gap-2 px-2 py-1 hover:bg-zinc-400'>
-                    <FaPen />
-                    Change name
-                  </li>
-                  <li className='flex justify-end px-2 py-1'>
-                    <div className='flex items-center gap-2 rounded-lg px-2 hover:bg-zinc-400'>
-                      <FaRightFromBracket />
-                      Sign out
-                    </div>
-                  </li>
-                </ul>
-              </div>
+              {showBox && (
+                <div className='absolute -left-52 w-60 rounded-md bg-zinc-600'>
+                  <ul>
+                    <li className='truncate border-b-1 border-zinc-400 px-2 py-1'>{name}</li>
+                    <li className='flex items-center gap-2 px-2 py-1 hover:bg-zinc-400'>
+                      <FaPen />
+                      Change name
+                    </li>
+                    <li className='flex justify-end px-2 py-1'>
+                      <div className='flex items-center gap-2 rounded-lg px-2 hover:bg-zinc-400'>
+                        <FaRightFromBracket />
+                        Sign out
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              )}
             </div>
           ) : (
             <button className='bg-reiseorange rounded-lg px-2 py-1 font-bold hover:bg-orange-400'>
