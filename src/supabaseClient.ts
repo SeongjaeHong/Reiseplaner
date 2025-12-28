bd80ca530cb1a21244215654665f90acc2dd8844
SeongjaeHong <sjhong310@gmail.com>
2025-12-29 02:44:06 +0900

    fix: remove a cookie storage to use Google OAuth sign-in

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,30 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './database.types';
 
-const isSecure = window.location.protocol === 'https:';
-
-const cookieStorage = {
-  getItem: (key: string) => {
-    const value = document.cookie.split('; ').find((row) => row.startsWith(`${key}=`));
-    return value ? decodeURIComponent(value.split('=')[1]) : null;
-  },
-  setItem: (key: string, value: string) => {
-    // Set a session cookie. This should be expired after the browser closed.
-    const secureFlag = isSecure ? '; Secure' : '';
-    document.cookie = `${key}=${encodeURIComponent(value)}; path=/; SameSite=Lax${secureFlag}`;
-  },
-  removeItem: (key: string) => {
-    document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-  },
-};
-
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string;
-const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
-  auth: {
-    storage: cookieStorage,
-    persistSession: true,
-  },
-});
+
+// You can use only local storage in the CSR app.
+// https://github.com/SeongjaeHong/Reiseplaner/wiki/PKCE-flow-in-OAuth-needs-local-storage-in-the-CSR-app.
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default supabase;
