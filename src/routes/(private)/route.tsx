619c5ae3e26b91e61a3271b80c77c3eb295ad38f
SeongjaeHong <sjhong310@gmail.com>
2025-12-28 23:27:44 +0900

    feat: add change the user name

--- src/routes/(private)/route.tsx
+++ src/routes/(private)/route.tsx
@@ -1,6 +1,13 @@
+import { signOut } from '@/apis/supabase/auth';
+import { getUser, updateUserName } from '@/apis/supabase/users';
 import { AuthContext } from '@/components/auth/AuthContext';
+import InputPopupBox from '@/components/common/popupBoxes/InputPopupBox';
+import { toast } from '@/components/common/Toast/toast';
+import useClickOutside from '@/utils/useClickOutside';
+import type { User } from '@supabase/supabase-js';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { createFileRoute, Link, Outlet } from '@tanstack/react-router';
-import { useContext, useReducer } from 'react';
+import { useContext, useState } from 'react';
 import { FaPen, FaRightFromBracket, FaUser } from 'react-icons/fa6';
 
 export const Route = createFileRoute('/(private)')({
@@ -8,10 +15,14 @@ export const Route = createFileRoute('/(private)')({
 });
 
 function RouteComponent() {
-  const { user } = useContext(AuthContext);
-  const [showBox, toggleShowBox] = useReducer((prev) => !prev, false);
+  const { user: sessionn } = useContext(AuthContext);
+  const { data: user } = useGetUser(sessionn);
+  const [showBox, setShowBox] = useState(false);
+  const [showNameBox, setShowNameBox] = useState(false);
+  const handleChangeName = useChangeUserName(user?.user_id);
 
-  const name = (user?.user_metadata?.display_name as string) ?? user?.email;
+  const refOutsideClick = useClickOutside();
+
   return (
     <>
       <div className='bg-reiseblue flex h-20 items-center justify-between px-5'>
@@ -20,29 +31,48 @@ function RouteComponent() {
         </Link>
         <div className='flex w-50 items-center justify-end gap-2 max-[540px]:text-sm'>
           <p className='max truncate font-bold text-orange-500 max-[470px]:w-30 max-[420px]:hidden'>
-            {name}
+            {user?.name}
           </p>
           {user ? (
             <div className='relative'>
-              <button onClick={toggleShowBox} className='bg-reiseorange rounded-full p-2 text-lg'>
+              <button
+                onClick={() => setShowBox(true)}
+                className='bg-reiseorange rounded-full p-2 text-lg'
+              >
                 <FaUser />
               </button>
               {showBox && (
-                <div className='absolute -left-52 w-60 rounded-md bg-zinc-600'>
+                <div
+                  ref={refOutsideClick(() => setShowBox(false))}
+                  className='absolute -left-52 w-60 rounded-md bg-zinc-600'
+                >
                   <ul>
-                    <li className='truncate border-b-1 border-zinc-400 px-2 py-1'>{name}</li>
-                    <li className='flex items-center gap-2 px-2 py-1 hover:bg-zinc-400'>
+                    <li className='truncate border-b-1 border-zinc-400 px-2 py-1'>{user?.name}</li>
+                    <li
+                      onClick={() => setShowNameBox(true)}
+                      className='flex items-center gap-2 px-2 py-1 hover:bg-zinc-400'
+                    >
                       <FaPen />
                       Change name
                     </li>
                     <li className='flex justify-end px-2 py-1'>
-                      <div className='flex items-center gap-2 rounded-lg px-2 hover:bg-zinc-400'>
+                      <div
+                        onClick={() => void signOut()}
+                        className='flex items-center gap-2 rounded-lg px-2 hover:cursor-pointer hover:bg-zinc-400'
+                      >
                         <FaRightFromBracket />
                         Sign out
                       </div>
                     </li>
                   </ul>
                 </div>
+              )}
+              {showNameBox && (
+                <InputPopupBox
+                  title='Change name'
+                  onAccept={handleChangeName}
+                  onClose={() => setShowNameBox(false)}
+                />
               )}
             </div>
           ) : (
@@ -58,3 +88,31 @@ function RouteComponent() {
     </>
   );
 }
+
+const useGetUser = (session: User | null) => {
+  return useQuery({
+    queryKey: ['user', session?.id],
+    queryFn: getUser,
+    enabled: !!session,
+  });
+};
+
+const useChangeUserName = (id: string | undefined) => {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: async (newName: string) => {
+      if (!id) {
+        throw new Error('User ID is required');
+      }
+
+      return await updateUserName(id, newName);
+    },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['user', id] }),
+    onError: () => {
+      toast.error('Failed to change a name.');
+    },
+  });
+
+  return mutate;
+};
