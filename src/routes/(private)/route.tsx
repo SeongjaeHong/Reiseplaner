8e5a5f4ef36d1e7aec69c11dce61eb6e3cf90844
SeongjaeHong <sjhong310@gmail.com>
2025-12-29 01:39:41 +0900

    chore: disable chabge name when user is a guest

--- src/routes/(private)/route.tsx
+++ src/routes/(private)/route.tsx
@@ -1,27 +1,50 @@
 import { signOut } from '@/apis/supabase/auth';
-import { getUser, updateUserName } from '@/apis/supabase/users';
+import { getUser, isGuestId, updateUserName } from '@/apis/supabase/users';
 import { AuthContext } from '@/components/auth/AuthContext';
 import InputPopupBox from '@/components/common/popupBoxes/InputPopupBox';
 import { toast } from '@/components/common/Toast/toast';
 import useClickOutside from '@/utils/useClickOutside';
 import type { User } from '@supabase/supabase-js';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { createFileRoute, Link, Outlet } from '@tanstack/react-router';
-import { useContext, useState } from 'react';
+import { createFileRoute, Link, Outlet, redirect } from '@tanstack/react-router';
+import { useContext, useRef, useState } from 'react';
 import { FaPen, FaRightFromBracket, FaUser } from 'react-icons/fa6';
 
 export const Route = createFileRoute('/(private)')({
   component: RouteComponent,
+  beforeLoad: ({ context }) => {
+    const { user, loading } = context.auth;
+
+    if (loading) {
+      // supabase session has not been read yet.
+      console.log('Session 로딩 중...');
+      return;
+    }
+
+    if (!user) {
+      return redirect({ to: '/signin' });
+    }
+  },
 });
 
 function RouteComponent() {
-  const { user: sessionn } = useContext(AuthContext);
-  const { data: user } = useGetUser(sessionn);
+  const { user: session } = useContext(AuthContext);
+  const { data: user } = useGetUser(session);
   const [showBox, setShowBox] = useState(false);
   const [showNameBox, setShowNameBox] = useState(false);
+  const isGuest = isGuestId(user?.user_id);
+
   const handleChangeName = useChangeUserName(user?.user_id);
+  const handleShowNameBox = () => {
+    if (isGuest) {
+      return;
+    }
+
+    setShowBox(true);
+  };
 
   const refOutsideClick = useClickOutside();
+  const refProfile = useRef<HTMLButtonElement>(null);
 
   return (
     <>
@@ -29,28 +52,29 @@ function RouteComponent() {
         <Link to={'/'}>
           <h1 className='text-5xl font-bold max-[540px]:text-4xl'>Reiseplaner</h1>
         </Link>
-        <div className='flex w-50 items-center justify-end gap-2 max-[540px]:text-sm'>
-          <p className='max truncate font-bold text-orange-500 max-[470px]:w-30 max-[420px]:hidden'>
-            {user?.name}
-          </p>
+        <div>
           {user ? (
-            <div className='relative'>
+            <div className='relative flex items-center justify-end gap-2 bg-red-500'>
+              <p className='max w-50 truncate text-right font-bold text-orange-500 max-[570px]:w-40 max-[540px]:text-sm max-[470px]:w-30 max-[420px]:hidden'>
+                {user.name}
+              </p>
               <button
-                onClick={() => setShowBox(true)}
+                ref={refProfile}
+                onClick={() => setShowBox((prev) => !prev)}
                 className='bg-reiseorange rounded-full p-2 text-lg'
               >
                 <FaUser />
               </button>
               {showBox && (
                 <div
-                  ref={refOutsideClick(() => setShowBox(false))}
-                  className='absolute -left-52 w-60 rounded-md bg-zinc-600'
+                  ref={refOutsideClick(() => setShowBox(false), [refProfile])}
+                  className='absolute top-0 -left-52 w-60 rounded-md bg-zinc-600'
                 >
                   <ul>
                     <li className='truncate border-b-1 border-zinc-400 px-2 py-1'>{user?.name}</li>
                     <li
-                      onClick={() => setShowNameBox(true)}
-                      className='flex items-center gap-2 px-2 py-1 hover:bg-zinc-400'
+                      onClick={handleShowNameBox}
+                      className={`flex items-center gap-2 px-2 py-1 ${isGuest ? 'text-zinc-500' : 'cursor-pointer hover:bg-zinc-400'}`}
                     >
                       <FaPen />
                       Change name
@@ -58,7 +82,7 @@ function RouteComponent() {
                     <li className='flex justify-end px-2 py-1'>
                       <div
                         onClick={() => void signOut()}
-                        className='flex items-center gap-2 rounded-lg px-2 hover:cursor-pointer hover:bg-zinc-400'
+                        className='flex cursor-pointer items-center gap-2 rounded-lg px-2 hover:bg-zinc-400'
                       >
                         <FaRightFromBracket />
                         Sign out
