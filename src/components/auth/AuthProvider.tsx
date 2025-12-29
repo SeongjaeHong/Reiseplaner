018e243b719f145fed4eb8616acf5831bee3344a
SeongjaeHong <sjhong310@gmail.com>
2025-12-29 18:16:40 +0900

    feat: sign-out when the app starts for the first time

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -2,16 +2,33 @@ import { useEffect, useState } from 'react';
 import supabase from '@/supabaseClient';
 import { AuthContext } from './AuthContext';
 import type { User } from '@supabase/supabase-js';
+import { signOut } from '@/apis/supabase/auth';
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    void supabase.auth.getSession().then(({ data }) => {
-      setUser(data.session?.user ?? null);
+    const wasAlive = sessionStorage.getItem('app-alive');
+
+    const init = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      // Ignore when the app starts by the page refresh
+      if (!wasAlive && session) {
+        // Sign out a local session as the app starts.
+        await signOut();
+      }
+
+      sessionStorage.setItem('app-alive', 'true');
+
+      setUser(session?.user ?? null);
       setLoading(false);
-    });
+    };
+
+    void init();
 
     const {
       data: { subscription },
