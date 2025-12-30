1150067fd7174a9d2bd370c1a9855fc591ed32b0
SeongjaeHong <sjhong310@gmail.com>
2025-12-31 01:29:30 +0900

    refactor: replace useContext to use

--- src/components/auth/AuthContext.tsx
+++ src/components/auth/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from 'react';
+import { createContext, use } from 'react';
 import type { User } from '@supabase/supabase-js';
 
 export type AuthState = {
@@ -9,7 +9,7 @@ export type AuthState = {
 export const AuthContext = createContext<AuthState | null>(null);
 
 export const useAuth = () => {
-  const context = useContext(AuthContext);
+  const context = use(AuthContext);
   if (!context) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
