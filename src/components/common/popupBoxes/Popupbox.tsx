44c00a615c12f7b3f3750a68af65760eb284f929
SeongjaeHong <sjhong310@gmail.com>
2026-01-03 16:39:42 +0900

    chore: change styles

--- src/components/common/popupBoxes/Popupbox.tsx
+++ src/components/common/popupBoxes/Popupbox.tsx
@@ -1,5 +1,3 @@
-import useClickOutside from '@/utils/useClickOutside';
-
 type PopupboxParams = {
   text: string;
   onAccept: () => void;
@@ -7,23 +5,27 @@ type PopupboxParams = {
 };
 
 export default function Popupbox({ text, onAccept, onCancel }: PopupboxParams) {
-  const outsideclick = useClickOutside();
-
   return (
-    <div
-      ref={outsideclick(onCancel)}
-      className='border-secondary fixed top-50 left-1/2 z-1 w-70 -translate-x-1/2 rounded-md border-2 bg-zinc-100 px-3 py-2'
-    >
-      <div>
-        <span className='text-secondary text-sm font-bold'>{text}</span>
-      </div>
-      <div className='flex w-full justify-center pt-3'>
-        <button className='bg-accent rounded-lg px-5 py-1' onClick={onCancel}>
-          Abbrechen
-        </button>
-        <button className='bg-accent mx-3 rounded-lg px-5 py-1' onClick={() => void onAccept()}>
-          OK
-        </button>
+    <div className='fixed inset-0 z-1 flex justify-center pt-50'>
+      <div onClick={onCancel} className='fixed inset-0' />
+      <div className='z-1 h-fit w-70 rounded-md bg-white px-3 py-2'>
+        <div className='mb-2'>
+          <span className='text-sm font-bold text-slate-800'>{text}</span>
+        </div>
+        <div className='flex w-full justify-center pt-3'>
+          <button
+            className='bg-primary hover:bg-primary-strong rounded-lg px-5 py-1'
+            onClick={onCancel}
+          >
+            Abbrechen
+          </button>
+          <button
+            className='bg-primary hover:bg-primary-strong mx-3 rounded-lg px-5 py-1'
+            onClick={onAccept}
+          >
+            OK
+          </button>
+        </div>
       </div>
     </div>
   );
