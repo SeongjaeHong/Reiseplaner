992e947ea1f6f7fb42de28d88c94b6b134aabe9f
SeongjaeHong <sjhong310@gmail.com>
2026-01-05 00:57:39 +0900

    chore: use cached image data

--- src/components/planContents/components/Editor/ImageComponent.tsx
+++ src/components/planContents/components/Editor/ImageComponent.tsx
@@ -1,7 +1,8 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { $getNodeByKey, type NodeKey } from 'lexical';
 import { ImageNode } from './ImageNode';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useFetchImage } from '@/utils/useFetchImage';
 
 export default function ImageComponent({
   src,
@@ -14,7 +15,6 @@ export default function ImageComponent({
   height: number;
   nodeKey: NodeKey;
 }) {
-  const [isLoading, setIsLoading] = useState(true);
   const [editor] = useLexicalComposerContext();
 
   const onResize = (newWidth: number, newHeight: number) => {
@@ -26,28 +26,46 @@ export default function ImageComponent({
     });
   };
 
+  const thumbnail = useFetchImage({ imageURL: src });
+  const [imgSrc, setImgSrc] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!thumbnail) return;
+
+    const objectUrl = URL.createObjectURL(thumbnail);
+    setImgSrc(objectUrl);
+
+    return () => {
+      URL.revokeObjectURL(objectUrl);
+      setImgSrc(null);
+    };
+  }, [thumbnail]);
+
   return (
     <div className='group/img relative h-fit max-w-full'>
-      {isLoading && (
+      {!imgSrc && (
         <div
           style={{ height: height }}
           className={`lounded-lg w-full animate-pulse rounded-lg bg-gray-300`}
         />
       )}
-      <img
-        src={src}
-        onLoad={() => setIsLoading(false)}
-        draggable={false}
-        className={`max-w-full rounded-lg ${isLoading ? 'hidden' : 'block'}`}
-      />
+      {imgSrc && (
+        <>
+          <img src={imgSrc} draggable={false} className='max-w-full rounded-lg' />
+          {/* image resizing handle */}
+          <div
+            className='invisible absolute right-0 bottom-0 h-4 w-4 translate-1/3 cursor-se-resize rounded-full bg-blue-500 group-hover/img:visible'
+            onMouseDown={(e) => startResize(e, width, height, onResize)}
+          />
+        </>
+      )}
 
-      {/* image resizing handle */}
-      {!isLoading && (
+      {/* {imgSrc && (
         <div
           className='invisible absolute right-0 bottom-0 h-4 w-4 translate-1/3 cursor-se-resize rounded-full bg-blue-500 group-hover/img:visible'
           onMouseDown={(e) => startResize(e, width, height, onResize)}
         />
-      )}
+      )} */}
     </div>
   );
 }
